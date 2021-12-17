ef065d69e8c3d57424ddb2e0d5a5408a12ec3a57
Robert Gibboni <robert@drivendata.org>
2021-12-17 17:35:53 -0500

    Placeholder submission test script

---
+++ runtime/scripts/test_submission.py
@@ -0,0 +1,7 @@
+# import pandas as pd
+
+# submission_format = pd.read_csv("/data/submission_format.csv")
+
+
+def test_submission_conforms_to_format():
+    pass
--- runtime/tests/test_submission.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-submission_format = pd.read_csv("/data/submission_format.csv")
-
-
-def test_submission_conforms_to_format():
-    pass
